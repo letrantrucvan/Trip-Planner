951655d07d95ef5be1d6991316427d014ef6c630
Lê Trần Trúc Vân <letrantrucvan@gmail.com>
2021-02-27 20:57:59 +0700

    Set Up GoogleMaps API
    illustrate maps, add marker

--- app/src/main/java/com/example/tripplanner/MainActivity.java
+++ app/src/main/java/com/example/tripplanner/MainActivity.java
@@ -4,11 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    GoogleMap map;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.myMap);
+        mapFragment.getMapAsync(this);
     }
-}
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        // Add a marker in Sydney, Australia,
+        // and move the map's camera to the same location.
+        LatLng sydney = new LatLng(-33.852, 151.211);
+
+        map.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}git
