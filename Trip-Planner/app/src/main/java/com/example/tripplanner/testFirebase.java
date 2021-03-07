96ea7af9642f0e60b232f449f8b41dbefd16cd0f
Minh Nguyễn <minh.nguyen28122000@gmail.com>
2021-03-07 13:47:01 +0700

    sign in qua email

--- Trip-Planner/app/src/main/java/com/example/tripplanner/testFirebase.java
+++ Trip-Planner/app/src/main/java/com/example/tripplanner/testFirebase.java
@@ -4,11 +4,19 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.ActionCodeSettings;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -27,10 +35,14 @@ public class testFirebase extends AppCompatActivity implements ValueEventListene
     private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
     private DatabaseReference ref = firebaseDatabase.getReference();
     private DatabaseReference Text = ref.child("Text");
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_firebase);
+
+        mAuth = FirebaseAuth.getInstance();
 
         btnsubmit = (Button) findViewById(R.id.btnsubmit);
         edtext = (EditText) findViewById(R.id.tvtext);
@@ -38,25 +50,85 @@ public class testFirebase extends AppCompatActivity implements ValueEventListene
         btnsubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String data = edtext.getText().toString();
-                Text.setValue(data);
+//                String data = edtext.getText().toString();
+//                Text.setValue(data);
+                logIn();
             }
         });
-
-        // Create a Map to store the data we want to set
-        Map<String, Object> docData = new HashMap<>();
-        docData.put("name", "Los Angeles");
-        docData.put("state", "CA");
-        docData.put("country", "USA");
-        docData.put("regions", Arrays.asList("west_coast", "socal"));
-        // Add a new document (asynchronously) in collection "cities" with id "LA"
-        ApiFuture<WriteResult> future = db.collection("cities").document("LA").set(docData);
-        // ...
-        // future.get() blocks on response
-        System.out.println("Update time : " + future.get().getUpdateTime());
+//        User a = new User("Trúc Vân", "letrantrucvan@gamil.com","vanmapdjt");
+//        User.addUser(a);
+//        User b = new User("Hoàng Minh", "minh@gmail.com","vanmapdjt");
+//        User.addUser(a);
+        signUp();
     }
 
+    private void authentication(){
+        ActionCodeSettings actionCodeSettings = ActionCodeSettings.newBuilder()
+                        // URL you want to redirect back to. The domain (www.example.com) for this
+                        // URL must be whitelisted in the Firebase Console.
+                        .setUrl("https://www.example.com/finishSignUp?cartId=1234")
+                        // This must be true
+                        .setHandleCodeInApp(true)
+                        .setIOSBundleId("com.example.ios")
+                        .setAndroidPackageName(
+                                "com.example.android",
+                                true, /* installIfNotAvailable */
+                                "12"    /* minimumVersion */)
+                        .build();
+    }
 
+    private void signUp(){
+        String email = "minh.nguyen28122000@gmail.com";
+        String password= "hellococo";
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            user.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    Toast.makeText(testFirebase.this, "Please check your email verification",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+                            Toast.makeText(testFirebase.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+    });
+    }
+
+    private void logIn(){
+        mAuth.signInWithEmailAndPassword("minh.nguyen28122000@gmail.com", "hellococo").addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    if (mAuth.getCurrentUser().isEmailVerified()){
+                        Toast.makeText(testFirebase.this, "Authentication successfully.",
+                                Toast.LENGTH_SHORT).show();
+                    } else{
+                        Toast.makeText(testFirebase.this, "Please verify your email.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+
+                } else {
+                    Toast.makeText(testFirebase.this, task.getException().getMessage(),
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
 
     @Override
     protected void onStart() {
