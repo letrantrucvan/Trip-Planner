529ce62a73bbdab5aa888ad2ee908ff23f815f9e
Lê Trần Trúc Vân <letrantrucvan@gmail.com>
2021-03-12 23:45:40 +0700

    Search Screen
    fetch data

---
+++ TravelPlanner/app/src/main/java/com/example/travelplanner/SearchResult.java
@@ -0,0 +1,110 @@
+package com.example.travelplanner;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+
+import com.example.travelplanner.controller.ToursViewHolder;
+import com.example.travelplanner.model.Tour;
+
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+
+
+public class SearchResult extends AppCompatActivity {
+    private EditText mSearchField;
+    private ImageButton mSearchBtn;
+
+    private RecyclerView mResultList;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    CollectionReference mTour;
+    private FirestoreRecyclerAdapter adapter;
+   // private DatabaseReference mTour;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_result);
+
+        mSearchField = (EditText) findViewById(R.id.search_input);
+        mSearchBtn = (ImageButton) findViewById(R.id.search_icon);
+        mResultList = (RecyclerView) findViewById(R.id.list_item);
+
+        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(this,DividerItemDecoration.VERTICAL);
+        mResultList.addItemDecoration(itemDecoration);
+
+        mResultList.setHasFixedSize(true);
+        mResultList.setLayoutManager(new LinearLayoutManager(this));
+
+
+//        firebaseUserSearch();
+        mSearchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String searchText = mSearchField.getText().toString();
+                firestoreUserSearch(searchText);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        //adapter.stopListening();
+    }
+
+    private void firestoreUserSearch(String text) {
+        //Query query =  db.collection("Tour");
+        Query searchQuery  = db.collection("Tour").orderBy("name").startAt(text).endAt(text+ '\uf8ff');
+        //Bind data
+        FirestoreRecyclerOptions<Tour> response = new FirestoreRecyclerOptions.Builder<Tour>()
+                .setQuery(searchQuery, Tour.class)
+                .build();
+
+        adapter = new FirestoreRecyclerAdapter<Tour, ToursViewHolder>(response) {
+                @Override
+                public void onBindViewHolder(ToursViewHolder holder, int position, Tour model) {
+                    holder.setDetail(model.getCover(), model.getName(), model.getDes());
+                }
+
+                @Override
+                public ToursViewHolder onCreateViewHolder(ViewGroup group, int i) {
+                    View mView = LayoutInflater.from(group.getContext())
+                            .inflate(R.layout.list_view, group, false);
+                    return new ToursViewHolder(mView);
+                }
+
+                @Override
+                public void onError(FirebaseFirestoreException e) {
+                    Log.e("error", e.getMessage());
+                }
+            };
+        adapter.notifyDataSetChanged();
+        adapter.startListening();
+        mResultList.setAdapter(adapter);
+        }
+    }
---
+++ TravelPlanner/app/src/main/java/com/example/travelplanner/controller/ToursViewHolder.java
@@ -0,0 +1,60 @@
+package com.example.travelplanner.controller;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.travelplanner.R;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+
+public class ToursViewHolder extends RecyclerView.ViewHolder{
+        View mView;
+
+        public ToursViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+
+
+        public void setDetail(String cover_link, String name_tour,String des_tour){
+            ImageView cover = (ImageView) mView.findViewById(R.id.cover);
+            TextView name  = (TextView) mView.findViewById(R.id.name);
+            TextView des = (TextView) mView.findViewById(R.id.des);
+
+            name.setText(name_tour);
+            des.setText(des_tour);
+
+            StorageReference imgRef = FirebaseStorage.getInstance().getReference().child(cover_link);
+            final long ONE_MEGABYTE = 1024 * 1024;
+            imgRef.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                @Override
+                public void onSuccess(byte[] bytes) {
+                    // Data for "images/island.jpg" is returns, use this as needed
+                    Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                    cover.setImageBitmap(bitmap);
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // Handle any errors
+                    System.out.println("Fail");
+                }
+            });
+
+        }
+
+    }
+
+
